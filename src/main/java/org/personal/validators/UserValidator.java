5303a547a570deaba444f62763b416be0d97e754
Vincent <vincentzhang@outlook.es>
2022-09-01 18:18:39 +0200

    add validation

---
+++ src/main/java/org/personal/validators/UserValidator.java
@@ -0,0 +1,18 @@
+package org.personal.validators;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+public class UserValidator implements Validator {
+
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return false;
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+
+    }
+}
